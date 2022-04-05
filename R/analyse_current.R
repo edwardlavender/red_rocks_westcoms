26f059e66be09b2ce96c790cf4e57426147cb066
Edward Lavender <el72@st-andrews.ac.uk>
2022-04-05 12:46:28 +0100

    Update analyse_current.R with updated MPA boundaries and to examine conditions in the MPA at large as well as in egg sites specifically.

--- R/analyse_current.R
+++ R/analyse_current.R
@@ -3,9 +3,7 @@
 #### analyse_current.R
 
 #### This script: 
-# 1) Analyses current speeds in the Red Rocks and Longay MPA
-# ... A maximum predicted current speed for the area over a one
-# ... year period is derived. 
+# 1) Analyses current speeds in the Red Rocks and Longay MPA & egg laying sites
 
 #### Steps preceding this script: 
 # 1) Define global parameters     (define_global_param.R)
@@ -20,22 +18,38 @@
 source("./R/define_global_param.R")
 
 #### Load data 
-mesh  <- readRDS("./data/spatial/mesh/mesh_around_elements_in_mpa.rds")
-raster::plot(mesh)
+mesh_mpa  <- readRDS("./data/spatial/mesh/mesh_around_elements_in_mpa.rds")
+mesh_reg  <- readRDS("./data/spatial/mesh/mesh_around_elements_in_region.rds")
+eggs      <- readRDS("./data/eggs/eggs.rds")
 
 
 ################################
 ################################
 #### Derive current speed estimates 
 
+#### Define analysis
+# mpa - extracts conditions in cells in MPA
+# eggs - extracts conditions with/without eggs (across a wider region)
+analysis <- "eggs" # mpa 
+stopifnot(analysis %in% c("mpa", "eggs"))
+if(analysis == "mpa"){
+  mesh <- mesh_mpa
+} else if(analysis == "eggs"){
+  mesh <- mesh_reg
+}
+raster::plot(mesh)
+
 #### Show that current speeds have been correctly estimated 
-# Load files 
-uv <- R.matlab::readMat(paste0(wc_root, "uvelocity/160301.mat"))$data
-vv <- R.matlab::readMat(paste0(wc_root, "vvelocity/160301.mat"))$data
-cs <- readRDS(paste0(wc_root, "current_speed/160301.rds"))
-# Define random index of bottom velocities to check 
-ind <- cbind(1:20, 10, 1:100)
-identical(sqrt(uv[ind]^2 + vv[ind]^2), cs[ind]) # TRUE
+check <- FALSE
+if(check){
+  # Load files 
+  uv <- R.matlab::readMat(paste0(wc_root, "uvelocity/160301.mat"))$data
+  vv <- R.matlab::readMat(paste0(wc_root, "vvelocity/160301.mat"))$data
+  cs <- readRDS(paste0(wc_root, "current_speed/160301.rds"))
+  # Define random index of bottom velocities to check 
+  ind <- cbind(1:20, 10, 1:100)
+  identical(sqrt(uv[ind]^2 + vv[ind]^2), cs[ind]) # TRUE
+}
 
 #### Define data for extraction 
 dates       <- seq(as.Date("2016-03-01"), as.Date("2017-02-28"), 1)
@@ -43,7 +57,20 @@ date_names  <- date_name(dates)
 hours       <- 0:23
 # Define layers (1 for surface or 10 for near seabed)
 layers      <- 10 # 1
-mesh_IDs    <- as.integer(as.character(mesh$ID))
+if(analysis == "mpa"){ 
+  mesh_IDs    <- as.integer(as.character(mesh$ID))
+} else if(analysis == "eggs"){
+  eggs$cell <-    
+    find_cells(eggs$lat, 
+               eggs$long, 
+               proj = raster::crs(mesh), 
+               mesh = mesh,
+               return = 4)
+  eggs$cell <- as.integer(as.character(eggs$cell))
+  table(eggs$cell[eggs$present == 1] %in% mesh_mpa$ID)
+  table(eggs$cell[eggs$present == 0] %in% mesh_mpa$ID)
+  mesh_IDs <- unique(eggs$cell)
+}
 wc <- 
   expand.grid(date_name = date_names, 
               hour = hours, 
@@ -62,30 +89,58 @@ if(run){
                            dir2load = paste0(wc_root, "/current_speed/"), 
                            extension = ".rds", 
                            cl = parallel::makeCluster(10L)
-  )
+                           )
   t2 <- Sys.time()
   difftime(t2, t1)
-  if(layers == 10){
-    saveRDS(wc, "./data/wc/current/wc_bot.rds")
-  } else if(layers == 1){
-    saveRDS(wc, "./data/wc/current/wc_sur.rds")
+  if(analysis == "mpa"){
+    if(layers == 10){
+      wc_bot <- wc
+      saveRDS(wc, "./data/wc/current/wc_bot.rds")
+    } else if(layers == 1){
+      wc_sur <- wc
+      saveRDS(wc, "./data/wc/current/wc_sur.rds")
+    }
+  } else if(analysis == "eggs"){
+    if(layers == 10){
+      wc_bot <- wc
+      saveRDS(wc, "./data/wc/current/wc_bot_eggs.rds")
+    } else if(layers == 1){
+      wc_sur <- wc
+      saveRDS(wc, "./data/wc/current/wc_sur_eggs.rds")
+    }
   }
 } else {
-  wc_bot <- readRDS("./data/wc/current/wc_bot.rds")
-  wc_sur <- readRDS("./data/wc/current/wc_sur.rds")
+  if(analysis == "mpa"){
+    wc_bot <- readRDS("./data/wc/current/wc_bot.rds")
+    # wc_sur <- readRDS("./data/wc/current/wc_sur.rds")
+  } else if(analysis == "eggs"){
+    wc_bot <- readRDS("./data/wc/current/wc_bot_eggs.rds")
+    # wc_sur <- readRDS("./data/wc/current/wc_sur_eggs.rds")
+  }
 }
 
 #### Estimate maximum current speeds 
-hist(wc_bot$wc)
-max(wc_bot$wc)
-# 0.1974771
-hist(wc_sur$wc)
-max(wc_sur$wc)
-# 0.7945567
-
-#### Examine ranges
-utils.add::basic_stats(wc_sur$wc)
 utils.add::basic_stats(wc_bot$wc)
+if(analysis == "eggs"){
+  cells_with_eggs <- unique(eggs$cell[eggs$present == 1])
+  utils.add::basic_stats(wc_bot$wc[wc_bot$mesh_ID %in% cells_with_eggs])
+}
+
+#### Examine annual trends
+## Define timestamps
+wc$date           <- date_name(wc$date_name, define = "date")
+wc$hour_char      <- as.character(wc$hour)
+pos               <- nchar(wc$hour_char) == 1
+wc$hour_char[pos] <- paste0("0", wc$hour_char[pos])
+wc$timestamp      <- fasttime::fastPOSIXct(paste0(wc$date, " ", wc$hour_char, ":00:00"), tz = "UTC")
+## Visualise trends 
+wc_by_hour <- 
+  wc %>%
+  dplyr::group_by(timestamp) %>%
+  dplyr::summarise(wc = median(wc))
+pretty_plot(wc_by_hour$timestamp, wc_by_hour$wc, type = "l")
+
+
 
 #### End of code. 
 ################################
